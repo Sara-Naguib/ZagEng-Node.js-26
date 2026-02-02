f2f34d3a56db889a42442a1e4743c883721c83b7
Sara Naguib <snaguib210@gmail.com>
2026-02-02 18:50:46 +0200

    task4

---
+++ js/task4/main.js
@@ -0,0 +1,122 @@
+// problrm 1: 
+
+// function toUpperCase(arr) {
+//   return arr.map(str => str.toUpperCase());
+// }
+
+// console.log(toUpperCase(["ahmed", "sara"]));
+
+console.log("**********************************************************\n");
+// problem 2
+
+// function checkPassed(students) {
+//   return students.map(student => {
+//     student.passed = student.score >= 50;
+//     return student;
+//   });
+// }
+
+
+// const input = [
+//   { name: "Ahmed", score: 80 },
+//   { name: "Sara", score: 40 }
+// ];
+
+// console.log(checkPassed(input));
+
+
+console.log("**********************************************************\n");
+// problem 3
+
+// function filterLongStrings(arr) {
+//   return arr.filter(str => str.length > 4);
+// }
+
+
+// console.log(filterLongStrings(["hello", "hi", "welcome"]));
+
+
+
+console.log("**********************************************************\n");
+// problem 4
+
+// function getAverage(numbers) {
+//   return numbers.reduce((a, b) => a + b) / numbers.length;
+// }
+
+// console.log(getAverage([10, 20, 30]));
+
+
+
+console.log("**********************************************************\n");
+// problem 5
+
+
+// function getAdultNames(people) {
+//   return people
+//     .filter(person => person.age >= 18)
+//     .map(person => person.name);
+// }
+
+// const input = [
+//   { name: "Ahmed", age: 23 },
+//   { name: "Sara", age: 16 },
+//   { name: "Mona", age: 19 }
+// ];
+
+// console.log(getAdultNames(input));
+
+
+console.log("**********************************************************\n");
+// problem 6
+
+// function fact(n) {
+//   let result = 1;
+
+//   for (let i = 1; i <= n; i++) {
+//     result *= i;
+//   }
+
+//   return result;
+// }
+
+
+// console.log(fact(5));
+// console.log(fact(4));
+
+
+
+
+console.log("**********************************************************\n");
+// problem 9
+
+
+// function replaceSpaces(str) {
+//   return str.split(" ").join("#");
+// }
+
+// console.log(replaceSpaces("Hello World"));
+
+
+console.log("**********************************************************\n");
+// problem 10
+
+
+// function modifyNumbers(arr) {
+//   let result = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//       result.push(arr[i] + 1);   
+//     } else {
+//       result.push(arr[i] - 1);   
+//     }
+//   }
+
+//   return result;
+// }
+
+
+// console.log(modifyNumbers([73, 221, 52, 214]));
+
+
